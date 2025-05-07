c805818286aeb9c74b98ac0b2a649b6fa936369b
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2025-05-07 19:16:03 +0200

    Fix port expose

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,12 +6,11 @@ const User = require("../model/user");
 router.post("/", upload.single("image"), async (req, res) => {
   try {
     // Upload image to cloudinary
-    const result = await cloudinary.uploader.upload(req.file.path);
+    //const result = await cloudinary.uploader.upload(req.file.path);
     // Create new user
     let user = new User({
       name: req.body.name,
-      avatar: result.secure_url,
-      cloudinary_id: result.public_id,
+
     });
     // Save user
     await user.save();
